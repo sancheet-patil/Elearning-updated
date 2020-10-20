9495efa047a9ade81389ade2cc7197948a1fca77
Sancheet Patil <61801632+sancheet-patil@users.noreply.github.com>
2020-10-20 13:17:55 +0530

    correction for previous year

--- app/previous_papers.php
+++ app/previous_papers.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class previous_papers extends Model
 {
 	 protected $table='previous_papers';
-    protected  $fillable=['Year','Name','Subject','Question','Option1','Option2','Option3','Option4','Answer'];
+    protected  $fillable=['Year','goal_id','course_id','subcourse_id','Question','Option1','Option2','Option3','Option4','Answer'];
 
     //
     public function goals()
